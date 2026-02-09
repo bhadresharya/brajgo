7d42290f4fb2784bfc91321e5c74aea9d8f47771
bhadresharya <bhadresharya@hotmail.com>
2026-02-09 22:35:57 +0530

    Update site configuration and enhance component links for dynamic base URL support;

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,12 @@
 import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
+const isProd = process.env.NODE_ENV === 'production';
+
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://bhadresharya.github.io/brajgo',
+	base: isProd ? '/brajgo' : '/',
 	vite: {
 		plugins: [tailwindcss()],
 	},
