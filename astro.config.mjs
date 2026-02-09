652aaf7e41c6059d478d16f61b070059c94de043
bhadresharya <bhadresharya@hotmail.com>
2026-02-09 22:39:32 +0530

    Refactor base URL handling across components and pages to remove trailing slashes for improved consistency in link generation.

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ const isProd = process.env.NODE_ENV === 'production';
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://bhadresharya.github.io/brajgo',
-	base: isProd ? '/brajgo' : '/',
+	base: isProd ? '/brajgo' : '',
 	vite: {
 		plugins: [tailwindcss()],
 	},
